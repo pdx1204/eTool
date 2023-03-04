b19ca4e5762d5cfdc8625f10078757155c47d7b7
Quincy Qiu <qiuqfang98@qq.com>
2023-03-04 19:36:27 +0800

    chore: 自动匹配 arco 的暗黑模式

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import ReactDOM from "react-dom/client";
-import "./styles/style.css";
-
-import "@arco-design/web-react/dist/css/arco.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
 import "./registers/screenshot";
+import "./styles/style.css";
+import "@arco-design/web-react/dist/css/arco.css";
+
+const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (darkThemeMq.matches) {
+  document.body.setAttribute("arco-theme", "dark");
+} else {
+  document.body.removeAttribute("arco-theme");
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <RouterProvider router={router} />
