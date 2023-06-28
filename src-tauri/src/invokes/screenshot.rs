3c2ef088c4c047a7ef7353ae417ec57e213acacf
qiuqfang <qiuqfang@outlook.com>
2023-06-28 18:54:39 +0800

    chore: screenshots 升级

--- src-tauri/src/invokes/screenshot.rs
+++ src-tauri/src/invokes/screenshot.rs
@@ -8,12 +8,12 @@ pub fn capture_full(position: PhysicalPosition<i32>) -> Vec<u8> {
     let screen = Screen::from_point(position.x, position.y).unwrap();
 
     let image = screen.capture().unwrap();
-    let buffer = image.buffer();
+    let buffer = image.to_png().unwrap();
     let home_path = home_dir().unwrap();
 
     let path = format!("{}/.eTool/screenshot.png", home_path.display());
     println!("{}", path);
-    fs::write(path, buffer).unwrap();
+    fs::write(path, buffer.clone()).unwrap();
     return buffer.to_vec();
 }
 
@@ -22,11 +22,11 @@ pub fn capture_region(x: i32, y: i32, width: u32, height: u32) -> Vec<u8> {
     let screen = Screen::from_point(x, y).unwrap();
 
     let image = screen.capture_area(x, y, width, height).unwrap();
-    let buffer = image.buffer();
+    let buffer = image.to_png().unwrap();
     let home_path = home_dir().unwrap();
 
     let path = format!("{}/.eTool/screenshot.png", home_path.display());
     println!("{}", path);
-    fs::write(path, buffer).unwrap();
+    fs::write(path, buffer.clone()).unwrap();
     return buffer.to_vec();
 }
