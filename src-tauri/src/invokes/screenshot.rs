e753718b804e6ddbae2717f0088a8ae5467253b4
qiuqfang <qiuqfang@outlook.com>
2023-08-01 10:39:18 +0800

    fix: to_png api param error

--- src-tauri/src/invokes/screenshot.rs
+++ src-tauri/src/invokes/screenshot.rs
@@ -8,7 +8,7 @@ pub fn capture_full(position: PhysicalPosition<i32>) -> Vec<u8> {
     let screen = Screen::from_point(position.x, position.y).unwrap();
 
     let image = screen.capture().unwrap();
-    let buffer = image.to_png().unwrap();
+    let buffer = image.to_png(None).unwrap();
     let home_path = home_dir().unwrap();
 
     let path = format!("{}/.eTool/screenshot.png", home_path.display());
@@ -22,7 +22,7 @@ pub fn capture_region(x: i32, y: i32, width: u32, height: u32) -> Vec<u8> {
     let screen = Screen::from_point(x, y).unwrap();
 
     let image = screen.capture_area(x, y, width, height).unwrap();
-    let buffer = image.to_png().unwrap();
+    let buffer = image.to_png(None).unwrap();
     let home_path = home_dir().unwrap();
 
     let path = format!("{}/.eTool/screenshot.png", home_path.display());
